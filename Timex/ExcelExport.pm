77f1c131b3e3e63969c9dfa3892976667260e258
Slaven Rezic <slaven@rezic.de>
2000-08-01 19:32:40 +0000

    template file: not absolute anymore new option -rate

--- Timex/ExcelExport.pm
+++ Timex/ExcelExport.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: ExcelExport.pm,v 1.1 2000/03/28 20:06:05 eserte Exp $
+# $Id: ExcelExport.pm,v 1.2 2000/08/01 19:32:40 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2000 Slaven Rezic. All rights reserved.
@@ -21,7 +21,7 @@ use Date::Calc qw(Week_Number);
 use strict;
 
 sub save_dialog {
-    my($top, $root_project) = @_;
+    my($top, $root_project, %args) = @_;
     my $t = $top->Toplevel;
     my $file;
     $t->title("Excel export");
@@ -70,7 +70,7 @@ sub save_dialog {
 		       my $from = timelocal @from_l;
 		       my $to   = timelocal @to_l;
 
-		       save($root_project, $file, $from, $to);
+		       save($root_project, $file, $from, $to, %args);
 
 		       $t->destroy;
 		   }
@@ -81,12 +81,12 @@ sub save_dialog {
 	       })->pack(-side => "left");
 }
 
-# XXX don't hardcode template file
 sub save {
-    my($root_project, $file, $from, $to) = @_;
+    my($root_project, $file, $from, $to, %args) = @_;
     my(@sub_projects) = $root_project->projects_by_interval($from, $to);
     my $name = ((getpwuid($<))[6]);
-    my $rate = 50; # XXX
+    my $rate = $args{-hourlyrate};
+    my $template_file = $args{-templatefile} || "Timex/de_template.csv";
     my(@l) = localtime $from;
     my $weeknumber = Week_Number($l[5]+1900, $l[4]+1, $l[3]);
     @l = localtime $to;
@@ -96,28 +96,33 @@ sub save {
     }
     my $sum = 0;
     open(S, ">$file") or die $!;
-    open(T, "$FindBin::RealBin/Timex/de_template.csv") or die $!; # XXX don't hardcode
+    $template_file = _find_template_file($template_file);
+    if (!$template_file) {
+	die "Can't find template_file in @INC";
+    }
+    open(T, $template_file) or die "Can't open $template_file: $!";
     while(<T>) {
 	if (/%%PROJECTNAME%%/) {
 	    my $template_line = $_;
 	    foreach my $p (@sub_projects) {
 		my $this_line = $template_line;
-		my $label = join("/", $p->path);
+		my $label = $p->pathname("/"); #join("/", $p->path);
 		$label =~ s|^/||; # strip root part
 		my $hours = $p->sum_time($from, $to)/(60*60);
-		my $rate  = $rate; # XXX
+		my $rate  = $rate;
 		my $projectsum = $hours * $rate;
 
 		# round rates and hours
 		$hours      = sprintf("%.1f", $hours);
 		$projectsum = sprintf("%.2f", $projectsum);
 
+		$sum += $projectsum;
+
 		# fix for german excel XXX
 		$hours       =~ s/\./,/;
 		$rate        =~ s/\./,/;
 		$projectsum  =~ s/\./,/;
 
-		$sum += $projectsum;
 		$this_line =~ s/%%PROJECTNAME%%/$label/g;
 		$this_line =~ s/%%HOURS%%/$hours/g;
 		$this_line =~ s/%%RATE%%/$rate/g;
@@ -134,6 +139,47 @@ sub save {
     close T;
     close S;
 }
+
+sub _find_template_file {
+    my $basename = shift;
+    if (file_name_is_absolute($basename)) {
+	return $basename;
+    } else {
+	foreach my $dir (@INC) {
+	    my $f = "$dir/$basename"; # XXX use File::Spec
+	    return $f if (-f $f and -r $f);
+	}
+    }
+    undef;
+}
+
+# REPO BEGIN
+# REPO NAME file_name_is_absolute /home/e/eserte/src/repository 
+# REPO MD5 47355e35bcf03edac9ea12c6f8fff9a3
+=head2 file_name_is_absolute($file)
+
+Return true, if supplied file name is absolute. This is only necessary
+for older perls where File::Spec is not part of the system.
+
+=cut
+
+sub file_name_is_absolute {
+    my $file = shift;
+    my $r;
+    eval {
+        require File::Spec;
+        $r = File::Spec->file_name_is_absolute($file);
+    };
+    if ($@) {
+	if ($^O eq 'MSWin32') {
+	    $r = ($file =~ m;^([a-z]:(/|\\)|\\\\|//);i);
+	} else {
+	    $r = ($file =~ m|^/|);
+	}
+    }
+    $r;
+}
+# REPO END
 
 1;
 
