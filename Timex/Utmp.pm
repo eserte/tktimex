59ee46af289f27242a9aab26c2278d2d2a8eea0d
Slaven Rezic <slaven@rezic.de>
2000-09-20 19:47:29 +0000

    linux compat

--- Timex/Utmp.pm
+++ Timex/Utmp.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: Utmp.pm,v 1.1 2000/09/20 02:01:52 eserte Exp $
+# $Id: Utmp.pm,v 1.2 2000/09/20 19:47:29 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2000 Slaven Rezic. All rights reserved.
@@ -16,6 +16,22 @@
 
 package Timex::Utmp;
 
+use strict;
+use vars qw($username_length $date_column $has_s);
+
+BEGIN {
+    if ($^O =~ /bsd/) {
+	$username_length = 16; # XXX check this!
+	$date_column = 43;
+	$has_s = 1;
+    } else { # e.g. linux
+	# XXX better to use User::Utmp if available
+	$username_length = 8;
+	$date_column = 39;
+	$has_s = 0;
+    }
+}
+
 sub new {
     my $pkg = shift;
     my $self = {};
@@ -28,13 +44,14 @@ sub init {
     $ENV{LC_ALL} = $ENV{LANG} = $ENV{LC_TIME} = "C";
     my @lines;
     my $this_year = (localtime)[5]+1900;
-    open(LAST, "last -s |");
+    my $cmd = "last " . ($has_s ? "-s " : "");
+    open(LAST, "$cmd|");
     while(<LAST>) {
 	next if (/^(reboot|shutdown)\b/); # XXX
 	chomp;
-	my $user = substr($_, 0, 16);
+	my $user = substr($_, 0, $username_length);
 	$user =~ s/\s+$//;
-	my $date = substr($_, 43, 16);
+	my $date = substr($_, $date_column, 16);
 	my $begin;
 	if ($date =~ /^(?:...)\s+(...)\s+(\d+)\s+(\d+):(\d+)/) {
 	    my $mon_abbrev = $1;
@@ -47,12 +64,17 @@ sub init {
 					    $day, $mon-1, $this_year-1900);
 	}
 	my $duration = 0;
-	if (/\(\s*(\d+)\)\s*$/) {
-	    $duration = $1;
-	    $end = $begin + $duration;
-	} elsif (/still logged in\s*$/) {
+	my $end;
+	if (/still logged in\s*$/) {
 	    $end = time;
 	    $duration = $end - $begin;
+	} else {
+	    if ($has_s && /\(\s*(\d+)\)\s*$/) {
+		$duration = $1;
+	    } elsif (!$has_s && /\(\s*(\d+):(\d+)\)\s*$/) {
+		$duration = $1*3600+$2*60;
+	    }
+	    $end = $begin + $duration;
 	}
 	push @lines, {User => $user, Begin => $begin, End => $end};
     }
