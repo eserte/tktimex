f35774408ada1edd2b75354db2834b845a829ccc
Slaven Rezic <slaven@rezic.de>
2003-03-28 16:52:29 +0000

    new options -r, -changed

--- stattimex.pl
+++ stattimex.pl
@@ -2,13 +2,13 @@
 # -*- perl -*-
 
 #
-# $Id: stattimex.pl,v 1.1 2002/03/15 19:09:07 eserte Exp $
+# $Id: stattimex.pl,v 1.2 2003/03/28 16:52:29 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2002 Online Office Berlin. All rights reserved.
 #
-# Mail: info@onlineoffice.de
-# WWW:  http://www.onlineoffice.de
+# Mail: slaven@rezic.de
+# WWW:  http://www.rezic.de/eserte/
 #
 
 use FindBin;
@@ -20,12 +20,16 @@ use strict;
 
 my $timexfile = "$ENV{HOME}/private/log/mytimex.pj1";
 my $project;
+my $recursive_project;
 my $verbose = 1;
 my $filelist;
+my $changed_lines;
 
 if (!GetOptions("timexfile|tf=s" => \$timexfile,
 		"v!" => \$verbose,
 		"filelist=s" => \$filelist,
+		"r!" => \$recursive_project,
+		"changed!" => \$changed_lines,
 	       )) {
     die "usage";
 }
@@ -55,22 +59,23 @@ foreach my $f (@files) {
 	warn "$f...\n";
     }
 
-    open(RLOG, "rlog '$f' | grep '^date:.*' |");
-    while(<RLOG>) {
-	if (/date:\s+(\S+\s+\S+\d)/) {
-	    my $rcsdate = $1;
-	    my $epoch = Timex::Rcs::Revision::rcsdate2unixtime($rcsdate);
-	    my $pluslines = 0;
-	    if (/lines:\s+\+(\d+)\s+-\d+/) {
-		$pluslines = $1;
+    if ($changed_lines) {
+	open(RLOG, "rlog '$f' | grep '^date:.*' |");
+	while(<RLOG>) {
+	    if (/date:\s+(\S+\s+\S+\d)/) {
+		my $rcsdate = $1;
+		my $epoch = Timex::Rcs::Revision::rcsdate2unixtime($rcsdate);
+		my $pluslines = 0;
+		if (/lines:\s+\+(\d+)\s+-\d+/) {
+		    $pluslines = $1;
+		}
+		$totallines += $pluslines;
+		unshift @mods, [$epoch, $pluslines, $rcsdate];
+	    } else {
+		warn "Can't parse rlog line $_";
 	    }
-	    $totallines += $pluslines;
-	    unshift @mods, [$epoch, $pluslines, $rcsdate];
-	} else {
-	    warn "Can't parse rlog line $_";
 	}
-    }
-    close RLOG;
+	close RLOG;
 
 #      for my $i (0 .. $#mods-1) {
 #  	my $time = $p->sum_time($mods[$i]->[0], $mods[$i+1]->[0]);
@@ -80,9 +85,31 @@ foreach my $f (@files) {
 #  	    warn "??? $mods[$i+1]->[1] lines in no time $mods[$i+1]->[2] ???\n";
 #  	}
 #      }
+
+    } else {
+	if ($f =~ /,v$/) {
+	    chomp(my $lines = `co -p $f | wc -l`);
+	    $totallines += $lines;
+	} else {
+	    chomp(my $lines = `cat $f | wc -l`);
+	    $totallines += $lines;
+	}
+    }
 }
 
-my $time = $p->sum_time(0);
-print "Project $project_path: " . int(86400*$totallines/$time) . " lines/day\n";
+my @sum_time_args = (0);
+if ($recursive_project) {
+    push @sum_time_args, undef, -recursive => 1;
+}
+my $time = $p->sum_time(@sum_time_args);
+print <<EOF
+Statistics for project $project_path (working day = 8h):
+lines/day:  @{[ int((3600*8)*$totallines/$time) ]}
+lines/hour: @{[ int(3600*$totallines/$time) ]}
+lines:      $totallines
+days:       @{[ int($time/(3600*8)) ]}
+hours:      @{[ int($time/3600) ]}
+EOF
+
 
 __END__
